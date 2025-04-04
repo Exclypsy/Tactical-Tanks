f1029f42fda1057aa3bf5a86833a11e78c0c5d26
Šimon Jedinák <simonjedinak009@gmail.com>
2025-04-04 17:14:13 +0200

    ?

--- client/game.py
+++ client/game.py
@@ -5,7 +5,7 @@ from Tank import Tank
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(800, 600, "Tank Game")
-        arcade.set_background_color(arcade.color.DESERT)
+        arcade.set_background_color(arcade.color.GRAY)
 
         # Create a sprite list to manage drawing
         self.sprites_list = arcade.SpriteList()
